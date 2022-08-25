616b408ecea4ac0c0daf14eb9315d4fc10868f98
uttam.konatham <uttam.konatham@gmail.com>
2022-08-26 00:38:47 +0530

    complete tasklist app

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -11,12 +11,31 @@ contract TodoList {
 
     mapping (uint => Task) public tasks;
 
+    event taskCreated(
+        uint id,
+        string content,
+        bool completed
+    );
+
+    event taskCompleted(
+        uint id,
+        bool completed
+    );
+
     constructor() public {
-        createTask("checkout dappuniversity.com");
+        createTask("Check out dappuniversity.com");
     }
 
     function createTask(string memory _content) public {
         taskCount ++;
         tasks[taskCount] = Task(taskCount, _content, false);
+        emit taskCreated(taskCount, _content, false);
+    }
+
+    function toggleCompleted(uint _id) public {
+        Task memory _task = tasks[_id];
+        _task.completed = !_task.completed;
+        tasks[_id] = _task;
+        emit taskCompleted(_id, _task.completed);
     }
 }
